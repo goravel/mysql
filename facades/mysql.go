6577ff4ab45575bc97148b2e8a65d6266121dfe7
ALMAS <almas1992@126.com>
2025-02-18 17:11:04 +0800

    chore: optimize error handling (#12)

--- facades/mysql.go
+++ facades/mysql.go
@@ -1,26 +1,25 @@
 package facades
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/goravel/framework/contracts/database/driver"
 
 	"github.com/goravel/mysql"
 )
 
-func Mysql(connection string) driver.Driver {
+func Mysql(connection string) (driver.Driver, error) {
 	if mysql.App == nil {
-		log.Fatalln("please register Mysql service provider")
-		return nil
+		return nil, fmt.Errorf("please register Mysql service provider")
 	}
 
 	instance, err := mysql.App.MakeWith(mysql.Binding, map[string]any{
 		"connection": connection,
 	})
 	if err != nil {
-		log.Fatalln(err)
-		return nil
+
+		return nil, err
 	}
 
-	return instance.(*mysql.Mysql)
+	return instance.(*mysql.Mysql), nil
 }
