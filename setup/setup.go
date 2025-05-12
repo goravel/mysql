4a8fdfeba346676ce3df152caf3e2255e43d6bea
almas1992 <almas1992@126.com>
2025-05-12 15:13:52 +0800

    feat: [#595] add support for package install/uninstall via artisan commands

---
+++ setup/setup.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"os"
+
+	"github.com/goravel/framework/packages"
+	"github.com/goravel/framework/packages/match"
+	"github.com/goravel/framework/packages/modify"
+	"github.com/goravel/framework/support/path"
+)
+
+var config = `map[string]any{
+        "host":     config.Env("DB_HOST", "127.0.0.1"),
+        "port":     config.Env("DB_PORT", 3306),
+        "database": config.Env("DB_DATABASE", "forge"),
+        "username": config.Env("DB_USERNAME", ""),
+        "password": config.Env("DB_PASSWORD", ""),
+        "charset":  "utf8mb4",
+        "loc":      "UTC", // Asia/Shanghai
+        "prefix":   "",
+        "singular": false,
+        "via": func() (driver.Driver, error) {
+            return mysqlfacades.Mysql("mysql")
+        },
+    }`
+
+func main() {
+	packages.Setup(os.Args).
+		Install(
+			modify.GoFile(path.Config("app.go")).
+				Find(match.Imports()).Modify(modify.AddImport(packages.GetModulePath())).
+				Find(match.Providers()).Modify(modify.Register("&mysql.ServiceProvider{}", "&queue.ServiceProvider{}")),
+			modify.GoFile(path.Config("database.go")).
+				Find(match.Imports()).Modify(modify.AddImport("github.com/goravel/framework/contracts/database/driver"), modify.AddImport("github.com/goravel/mysql/facades", "mysqlfacades")).
+				Find(match.Config("database.connections")).Modify(modify.AddConfig("mysql", config)),
+		).
+		Uninstall(
+			modify.GoFile(path.Config("app.go")).
+				Find(match.Providers()).Modify(modify.Unregister("&mysql.ServiceProvider{}")).
+				Find(match.Imports()).Modify(modify.RemoveImport(packages.GetModulePath())),
+			modify.GoFile(path.Config("database.go")).
+				Find(match.Config("database.connections")).Modify(modify.RemoveConfig("mysql")).
+				Find(match.Imports()).Modify(modify.RemoveImport("github.com/goravel/framework/contracts/database/driver"), modify.RemoveImport("github.com/goravel/mysql/facades", "mysqlfacades")),
+		).
+		Execute()
+}
